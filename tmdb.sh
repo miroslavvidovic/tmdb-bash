17b24ef0e3826bb7c2d6f431b9583fe7c9dd1e22
Miroslav Vidović <miroslav-vidovic@hotmail.com>
2017-07-20 21:11:47 +0200

    Improved input parsing, help section and fixing shellcheck issues

--- tmdb.sh
+++ tmdb.sh
@@ -2,57 +2,64 @@
 
 # -----------------------------------------------------------------------------
 # Info:
-# 	Miroslav Vidovic
-# 	tmdb.sh
-# 	06.10.2016.-13:29:29
+#   author:    Miroslav Vidovic
+#   file:      tmdb.sh
+#   created:   06.10.2016.-13:29:29
+#   revision:  20.07.2017.
+#   version:   1.1
 # -----------------------------------------------------------------------------
+# Requirements:
+#   curl, xdg-open, jq
 # Description:
-#
+#   Get some basic movie information from the moviedb website api.
 # Usage:
-#
+#   Detailed usage description in the help function.
 # -----------------------------------------------------------------------------
 # Script:
 
-APIKEY=$(cat apikey.txt)
+# Get the api key and insert it here
+APIKEY="your api key"
 
+SCRIPTNAME=$(basename "$0")
+
+help(){
+  echo "Usage :  $SCRIPTNAME [options] title 
+
+    title        Title of the movie
+
+    Options:
+    -y|year      Optional year for better search results
+    -i|image     Display the movie poster as an image
+    -h|help      Display this help message
+    "
+}
+
+# Curl request on the tmdb api
 get_movie_info(){
-  curl --request GET \
+  curl -s --request GET \
     --url "https://api.themoviedb.org/3/search/movie?year=$2&query=$1&language=en-US&api_key=$APIKEY" \
     --header 'content-type: application/json' \
     --data '{}'
 }
 
 show_poster(){
- eog http://image.tmdb.org/t/p/w1000/$1
-}
-
-help(){
-  echo "Usage :  $0 [options] [--]
-
-    Options:
-    -t|title
-    -y|year
-    -i|image
-    -h|help       Display this message
-    "
+ xdg-open http://image.tmdb.org/t/p/w1000/"$1"
 }
 
 main(){
   image=false
 
-  while getopts "t:y:ih" flag; do
+  while getopts "y:ih" flag; do
     case "${flag}" in
-      t|title)
-        title="${OPTARG}"
-          ;;
       y|year)
         year=${OPTARG}
           ;;
       i|image)
         image=true
           ;;
-      h|help) help
+      h|help)
         help
+        exit 0
           ;;
       *)
         echo -e "\n Option does not exist : $OPTARG\n"
@@ -61,18 +68,29 @@ main(){
           ;;
     esac
   done
+  shift $((OPTIND-1))
 
-  formated_title=$(echo $title | sed 's/ /%20/g')
-  echo $formated_title
+  title="$1"
 
-  data=$(get_movie_info $formated_title $year)
-  echo $data | jq
+  # If no movie title is supplied display help and exit
+  if [[ ! $title ]]; then
+    echo -e "Error: A movie title is required\n"
+    help
+    exit 1
+  fi
 
-  if $image; then
-    image_url=$(echo $data | jq '.results[0] .poster_path')
-    filtered_url=$(echo $image_url | tr -d "\"")
-    echo $filtered_url
-    show_poster $filtered_url
+  # Change all spaces in the title to "%20" for the URL
+  formated_title=${title// /%20}
+
+  data=$(get_movie_info "$formated_title" "$year")
+  echo "$data" | jq '.'
+
+  # If the image option is requested
+  if "$image"; then
+    image_url=$(echo "$data" | jq '.results[0] .poster_path')
+    filtered_url=$(echo "$image_url" | tr -d "\"")
+    echo "$filtered_url"
+    show_poster "$filtered_url"
   fi
 }
 
